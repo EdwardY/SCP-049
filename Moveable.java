3c6920b9c2027e91dd3320286e9c3d2b7d2f9869
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-12-24 12:50:03 -0500

    updated moveable, added wholegameevent

--- Moveable.java
+++ Moveable.java
@@ -7,26 +7,9 @@
 
 public interface Moveable {
     /**
-     * Moves a distance amount up
-     * @param distance the distance to move
+     * Translates xDistance horizontally and yDistance verticalls
+     * @param xDistance the horizontal distance to translate
+     * @param yDistance the vertical distance to translate
      */
-    public void moveUp(int distance);
-
-    /**
-     * Moves a distance amount down
-     * @param distance the distance to move
-     */
-    public void moveDown(int distance);
-
-    /**
-     * Moves a distance amount left
-     * @param distance the distance to move
-     */
-    public void moveLeft(int distance);
-    
-    /**
-     * Moves a distance amount right
-     * @param distance the distance to move
-     */
-    public void moveRight(int distance);
+    public void translate(int xDistance, int yDistance);
 }
---
+++ WholeGameEvent.java
@@ -0,0 +1,3 @@
+abstract class WholeGameEvent extends Event{
+    
+}

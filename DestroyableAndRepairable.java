e5d28f8acc0315b72fd8011c45b2f2df21b5ab1e
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-12-24 12:02:07 -0500

    javadocs

--- DestroyableAndRepairable.java
+++ DestroyableAndRepairable.java
@@ -5,6 +5,14 @@
  * @version 1.0 on December 24, 2020
  */
 interface DestroyableAndRepairable {
+    /**
+     * Takes damage amount of damage by decreasing the health by damage amount
+     * @param damage the amount of damage to take
+     */
     public void takeDamage(int damage);
+    /**
+     * Adds repair amount of health back 
+     * @param repair the amount of health to repair by
+     */
     public void repair(int repair);
 }

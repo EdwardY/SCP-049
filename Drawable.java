b759e131b444005e681b6d96464479c8a9ae1f60
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-12-24 11:45:42 -0500

    Create Drawable.java

---
+++ Drawable.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+/**
+ * [Drawable.java]
+ * Interface that defines methods for classes which have stuff that need to be drawn
+ * @author Vivian Dai
+ * @version 1.0 on December 24, 2020
+ */
+interface Drawable{
+    /**
+     * Draws the things that need to be drawn on the {@code Graphics} g
+     * @param g the {@code Graphics} surface to be drawing on
+     */
+    public void draw(Graphics g);
+}

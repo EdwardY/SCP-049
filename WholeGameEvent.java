2338e6dbdceb6af84bcee3d19daf2f8ff36e4bfd
v341196137 <38384400+v341196137@users.noreply.github.com>
2020-12-24 14:12:15 -0500

    Update WholeGameEvent.java

--- WholeGameEvent.java
+++ WholeGameEvent.java
@@ -1,3 +1,23 @@
+/**
+ * [WholeGameEvent.java]
+ * The class for {@code Events} that affect the entire game
+ * @author Vivian Dai
+ * @version 1.0 on December 24, 2020
+ */
+
 abstract class WholeGameEvent extends Event{
-    
+    /**
+     * Passes all parameters into the superclass to be constructed
+     * @param duration the total duration of the {@code Event}
+     * @param timeLeft the time left in the {@code Event}
+     * @param effectAmount the amount of effect the {@code Event} has
+     */
+    public WholeGameEvent(int duration, int timeLeft, int effectAmount){
+        super(duration, timeLeft, effectAmount);
+    }
+
+    /**
+     * Does the effect
+     */
+    public abstract void effect();
 }

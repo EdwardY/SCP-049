a83c811968840eb140c7ddf6a46f3efd177dc354
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-01-06 13:23:05 -0500

    server accepts 2 clients

--- Server.java
+++ Server.java
@@ -19,6 +19,8 @@ import java.util.ArrayList;
  */
 
 class Server {
+    /** The {@code ServerSocket} for where all the networking will be run */
+    ServerSocket serverSock;
     /** The port to connect to */
     private int port;
     /** Handles town player actions */
@@ -38,14 +40,58 @@ class Server {
 
     /**
      * Creates the {@code Server}
-     * @param port
+     * @param port the port to use
      */
     public Server(int port){
         this.port = port;
         this.game = new Game();
+        this.scp = null;
+        this.town = null;
     }
 
-    //TODO: accept 2 clients (randomly assign first one)
+    /**
+     * <p>
+     * Starts looking for connections. Will keep looking for connections as long as there isn't the requirement of two players. 
+     * If there are no players in the game, SCP or town role will be assigned randomly. If there already is a player then the 
+     * newly joined player gets the not taken role. 
+     * </p>
+     */
+    public void go(){
+        Socket client = null;
+        try{
+            serverSock = new ServerSocket(this.port);
+            while((this.scp == null) || (this.town == null)){
+                client = serverSock.accept();
+                if((this.scp == null) && (this.town == null)){
+                    //assign at random
+                    double rand = Math.random();
+                    if(rand > 0.5){
+                        this.scp = new ClientHandler(client);
+                        this.scpThread = new Thread(this.scp);
+                    }else{
+                        this.town = new ClientHandler(client);
+                        this.townThread = new Thread(this.scp);
+                    }
+                }else if(this.scp == null){
+                    //assigns scp
+                    this.scp = new ClientHandler(client);
+                    this.scpThread = new Thread(this.scp);
+                }else{
+                    //assigns town
+                    this.town = new ClientHandler(client);
+                    this.townThread = new Thread(this.scp);
+                }
+            }
+        }catch(Exception e) { 
+            System.out.println("Error accepting connection");
+        }
+        try {
+          client.close();
+        }catch (Exception e1) { 
+            System.out.println("Failed to close socket");
+        }
+        System.exit(-1);
+    }
 
     /**
      * [ClientHandler.java]
@@ -54,6 +100,22 @@ class Server {
      * @version 1.0 on January 5, 2021
      */
     private class ClientHandler implements Runnable{
+        /** The {@code PrintWriter} used to send stuff over to the client's computer */
+        private PrintWriter output;
+        /** The stream for network input */
+        private BufferedReader input;
+        /** {@code Socket} which the client uses */
+        private Socket client;
+        private ClientHandler(Socket s){
+            this.client = s;
+            try {
+                this.output = new PrintWriter(client.getOutputStream());
+                InputStreamReader stream = new InputStreamReader(client.getInputStream());
+                this.input = new BufferedReader(stream);
+            }catch(IOException e) {
+                e.printStackTrace();        
+            }
+        }
         public void run(){
             //TODO: actually handle clients
         }

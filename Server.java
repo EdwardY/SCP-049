cb87859d3e71675209373696a6531a420dba04e1
v341196137 <38384400+v341196137@users.noreply.github.com>
2021-01-22 20:34:37 -0500

    shoot me

--- Server.java
+++ Server.java
@@ -641,7 +641,13 @@ class Server {
                         sendTo(allUsers, "" + game.getBuildings().size());
                         for(int i = 0;i < game.getBuildings().size();i++){
                             Building curBuilding = game.getBuildings().get(i);
-                            sendTo(allUsers, curBuilding.getClass().getSimpleName() + " " + curBuilding.getMaxHealth() + " " + curBuilding.getHealth() + " " + curBuilding.getX() + " " + curBuilding.getY());
+                            String send = curBuilding.getClass().getSimpleName() + " " + curBuilding.getMaxHealth() + " " + curBuilding.getHealth() + " " + curBuilding.getX() + " " + curBuilding.getY();
+                            if(curBuilding instanceof Residency){
+                                send = send +  " " + ((Residency)curBuilding).getMaxCap();
+                            }else if(curBuilding instanceof Hospital){
+                                send = send + " " + ((Hospital)curBuilding).getMaxCapacity();
+                            }
+                            sendTo(allUsers, send);
                         }
 
                         //humans
